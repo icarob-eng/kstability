e8ba0dcc8765ca633323765a6042b06485745fa9
Ícaro Bruno <ibsc013@gmail.com>
2023-08-09 12:43:29 -0300

    Updates all parsing and serializing methods to choose language

--- src/commonMain/kotlin/com/moon/kstability/parsing/ModelSerializers.kt
+++ src/commonMain/kotlin/com/moon/kstability/parsing/ModelSerializers.kt
@@ -3,40 +3,38 @@ package com.moon.kstability.parsing
 import com.moon.kstability.*
 
 object ModelSerializers {
-    private val s = StringsPtBrObject
-
-    fun Vector.serialize(): Map<String, Float> = mapOf(s.x to x, s.y to y)
+    fun Vector.serialize(lang: YamlStrings): Map<String, Float> = mapOf(lang.x to x, lang.y to y)
 
     fun Beam.serialize(): List<String> = listOf(node1.name, node2.name)
 
-    fun Support.serialize(): Pair<String, Map<String, Any>> =
+    fun Support.serialize(lang: YamlStrings): Pair<String, Map<String, Any>> =
         Pair(node.name, mapOf(
-            s.gender to gender.reactions,
-            s.direction to when (direction) {
-                Vector.VERTICAL -> s.vertical
-                Vector.HORIZONTAL -> s.horizontal
-                else -> direction.serialize()
+            lang.gender to gender.reactions,
+            lang.direction to when (direction) {
+                Vector.VERTICAL -> lang.vertical
+                Vector.HORIZONTAL -> lang.horizontal
+                else -> direction.serialize(lang)
             }
         ))
 
-    fun Node.serialize(): Pair<String, Map<String, Float>> = Pair(name, pos.serialize())
+    fun Node.serialize(lang: YamlStrings): Pair<String, Map<String, Float>> = Pair(name, pos.serialize(lang))
 
-    fun PointLoad.serialize(): Map<String, Any> = mapOf(
-        s.node to node.name,
-        s.vector to vector.serialize()
+    fun PointLoad.serialize(lang: YamlStrings): Map<String, Any> = mapOf(
+        lang.node to node.name,
+        lang.vector to vector.serialize(lang)
     )
 
-    fun DistributedLoad.serialize(): Map<String, Any> = mapOf(
-        s.nodes to listOf(node1.name, node2.name),
-        s.vector to vector.serialize()
+    fun DistributedLoad.serialize(lang: YamlStrings): Map<String, Any> = mapOf(
+        lang.nodes to listOf(node1.name, node2.name),
+        lang.vector to vector.serialize(lang)
     )
 
-    fun Structure.serialize(): Map<String, Any> = mapOf(
-        s.structureSection to name,
-        s.nodesSection to nodes.associate { it.serialize() },
-        s.supportSection to getSupports().associate { it.serialize() },
-        s.beamsSection to getBeams().map { it.serialize() },
-        s.loadsSection to (getPointLoads().map { it.serialize() } + getDistributedLoads().map { it.serialize() })
+    fun Structure.serialize(lang: YamlStrings): Map<String, Any> = mapOf(
+        lang.structureSection to name,
+        lang.nodesSection to nodes.associate { it.serialize(lang) },
+        lang.supportSection to getSupports().associate { it.serialize(lang) },
+        lang.beamsSection to getBeams().map { it.serialize() },
+        lang.loadsSection to (getPointLoads().map { it.serialize(lang) } + getDistributedLoads().map { it.serialize(lang) })
             .mapIndexed { i, load -> Pair("F$i", load) }.toMap()
     )
 }
--- src/commonMain/kotlin/com/moon/kstability/parsing/YamlStrings.kt
+++ src/commonMain/kotlin/com/moon/kstability/parsing/YamlStrings.kt
@@ -1,39 +1,5 @@
 package com.moon.kstability.parsing
 
-object StringsPtBrObject {
-    // consts
-    const val vertical = "vertical"
-    const val horizontal = "horizontal"
-
-    // sections
-    const val structureSection = "estrutura"
-    const val supportSection = "apoios"
-    const val loadsSection = "cargas"
-    const val nodesSection = "nós"
-    const val beamsSection = "barras"
-
-    // attributes
-    const val x = "x"
-    const val y = "y"
-    const val direction = "direção"
-    const val gender = "gênero"
-    const val node = "nó"
-    const val nodes = "nós"
-    const val module = "módulo"
-    const val vector ="vetor"
-
-    // exceptions
-    const val invalidVector = "Invalid vector. Value = {}"
-    const val invalidNodeRef = "Referenced node \"{}\" not in the structure."
-    const val invalidSupportSyntax = "Invalid support syntax. Value = {}"
-    const val invalidSupportGender = "Invalid support gender. Value = {}"
-    const val invalidBeamList = "Invalid node quantity for a beam. Value = {}"
-    const val invalidLoadVectorSyntax = "Invalid load vector syntax. Value = {}"
-    const val invalidLoadNodesSyntax = "Invalid load syntax, $node or $nodes expected."
-    const val invalidLoadSyntax = "Invalid load syntax. Value = {}"
-    const val invalidSection = "Invalid {} section."
-}
-
 interface YamlStrings {
     // consts
     val vertical: String
